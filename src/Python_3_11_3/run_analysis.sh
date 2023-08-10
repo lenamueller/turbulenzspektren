96f47a10e5fb20510ff8fc3ac65c69f738d51f11
Lena Mueller <lenamariemue@gmail.com>
2023-08-10 18:15:13 +0200

    Move file to Python_3_11_3 dir

--- src/Python_3_11_3/run_analysis.sh
+++ src/Python_3_11_3/run_analysis.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 echo "Run analysis..."
-for measuring_situation in "ES_2023_07_08" "ES_2023_07_11" "GAS_2023_07_11"
+
+for measuring_situation in "ES_2023_07_08_morning" "ES_2023_07_08_noon" "ES_2023_07_08_afternoon" "ES_2023_07_11_morning" "GAS_2023_07_11_noon" "ES_2023_07_11_noon"
 do
     python fft.py $measuring_situation "EXPE"
     python fft.py $measuring_situation "SONIC"
-done
-
-for measuring_situation in "ES_2023_07_08" "ES_2023_07_11" "GAS_2023_07_11"
-do
     python averaging.py $measuring_situation "EXPE"
     python averaging.py $measuring_situation "SONIC"
 done
+
 echo "Done"
