1b52c17996ad61c809c0a288849b36c4330aec81
Lena Mueller <lenamariemue@gmail.com>
2023-08-10 15:41:22 +0200

    Add inheritance for Dataset classes.

--- code/Datasets.py
+++ code/Datasets.py
@@ -5,25 +5,71 @@ import pandas as pd
 from scipy import fft
 from scipy.signal import detrend
 
-    
-class ExpeDataset:
-    def __init__(
-            self, fn: str, 
-            start_time: str,
-            end_time: str) -> None:
+pd.options.mode.chained_assignment = None  # default='warn'
 
-        """Constructor"""
 
-        # general
-        self.date: str = fn[-21:-13]
+class Dataset:
+    def __init__(self, fn: str, start_time: str, end_time: str) -> None:
+        
+        self.fn: str = fn
         self.start_time: str = start_time
         self.end_time: str = end_time
-        print(f"Processing EXPE data {self.date} {self.start_time} {self.end_time}")
-        
-        self.time_raw: np.ndarray = None
-        self.nf: int = None
-        self.kernel_size: int = None
 
+        self.time_raw: np.ndarray = None
+        self.nf: int = None # Nyquist frequency
+        
+    def parse_data() -> None:
+        """empty method to be overwritten by subclasses """
+        pass 
+
+    def detrend_signal(self, var: np.ndarray) -> None:
+        """Detrend the time series."""
+        return np.mean(var) + detrend(var, type="linear")
+    
+    def calc_spectrum(self, var: np.ndarray, sample_rate: int = 1) -> None:
+        """Calculate the FFT of the time series."""
+        
+        # Calculate FFT
+        fft_output = fft.fft(var)
+        freqs = fft.fftfreq(len(var), 1/sample_rate)
+        spectrum = np.abs(fft_output)
+        
+        # Remove frequencies above Nyquist frequency
+        nf = floor(len(self.time_raw)/2)
+        freqs = freqs[:nf]
+        spectrum = spectrum[:nf]
+        
+        return freqs, spectrum
+
+    def smooth_spectrum(self, var: np.ndarray, kernel_size: int = 12) -> None:
+        """Smooth the time series with a moving average"""
+        kernel = np.ones(kernel_size) / kernel_size
+        return np.convolve(var, kernel, mode='valid')
+        
+    def step_mean(self, var: np.ndarray, window_size: int) -> list:
+        """Calculate the average of the time series of variable var in intervals of x minutes."""
+        l = []
+        for i in range(0, len(self.time_raw), window_size):
+            new_values = [np.mean(var[i:i+window_size])] * window_size
+            l.extend(new_values)
+        return l
+        
+    def rolling_mean(self, var: np.ndarray, window_size: int) -> list:
+        """Calculate the rolling mean of the time series of variable var with a window_size of x minutes."""
+        l = []
+        for i in range(len(var)):
+            if i < window_size:
+                l.append(np.mean(var[0:i+1]))
+            else:
+                l.append(np.mean(var[i-window_size:i]))
+        return l
+
+class ExpeDataset(Dataset):
+    # Dataset for expe data
+
+    def __init__(self, fn: str, start_time: str, end_time: str) -> None:
+        super().__init__(fn, start_time, end_time)
+        
         # temperature
         self.t_raw: np.ndarray = None
         self.t_det: np.ndarray = None
@@ -32,37 +78,45 @@ class ExpeDataset:
         self.t_spectrum_smooth: np.ndarray = None
 
         # relative humidity
-        self.rF_raw: np.ndarray = None
-        self.rF_det: np.ndarray = None
-        self.rF_freqs: np.ndarray = None
-        self.rF_spectrum: np.ndarray = None
-        self.rF_spectrum_smooth: np.ndarray = None
-
+        self.rH_raw: np.ndarray = None
+        self.rH_det: np.ndarray = None
+        self.rH_freqs: np.ndarray = None
+        self.rH_spectrum: np.ndarray = None
+        self.rH_spectrum_smooth: np.ndarray = None
+        
         # pressure
         self.p_raw: np.ndarray = None
         self.p_det: np.ndarray = None
         self.p_freqs: np.ndarray = None
         self.p_spectrum: np.ndarray = None
         self.p_spectrum_smooth: np.ndarray = None
-        
-        
+
         self.parse_data(fn, self.start_time, self.end_time)
-        self.detrend_signal()
-        self.calc_spectrum()
-        self.smooth_1d_arr()
+        
+        self.t_det = self.detrend_signal(var=self.t_raw)
+        self.rH_det = self.detrend_signal(var=self.rH_raw)
+        self.p_det = self.detrend_signal(var=self.p_raw)
+        
+        self.t_freqs, self.t_spectrum = self.calc_spectrum(var=self.t_det, sample_rate=1)
+        self.rH_freqs, self.rH_spectrum = self.calc_spectrum(var=self.rH_det, sample_rate=1)
+        self.p_freqs, self.p_spectrum = self.calc_spectrum(var=self.p_det, sample_rate=1)
+        
+        self.p_spectrum_smooth = self.smooth_spectrum(self.p_spectrum)
+        self.t_spectrum_smooth = self.smooth_spectrum(self.t_spectrum)
+        self.rH_spectrum_smooth = self.smooth_spectrum(self.rH_spectrum)
+        
         
     def parse_data(self, fn: str, start_time: str, end_time: str) -> None:
-        """Parse the data from the txt-file."""
     
         df = pd.read_csv(fn, delimiter=";")
 
         sensor0 = df.loc[df['Module Command'] == 0]
-        rename_cols = {"Value2":"T", "Value3":"rF", "Value4":"p"}
-        type_cols = {'T': float, 'rF': float, 'p': float}
+        rename_cols = {"Value2":"T", "Value3":"rH", "Value4":"p"}
+        type_cols = {'T': float, 'rH': float, 'p': float}
         sensor0 = sensor0.rename(columns=rename_cols).astype(type_cols)
 
         sensor0["T"] = sensor0["T"]/100
-        sensor0["rF"] = sensor0["rF"]/1000
+        sensor0["rH"] = sensor0["rH"]/1000
         sensor0["p"] = sensor0["p"]/1000
 
         sensor0["Datetime"] = sensor0['Date'].astype(str) +" "+ sensor0["Time"]
@@ -74,100 +128,53 @@ class ExpeDataset:
 
         self.time_raw = pd.to_datetime(sensor0_flt["Datetime"])
         self.t_raw = sensor0_flt["T"].to_numpy()
-        self.rF_raw = sensor0_flt["rF"].to_numpy()
+        self.rH_raw = sensor0_flt["rH"].to_numpy()
         self.p_raw = sensor0_flt["p"].to_numpy()
+   
 
-    def detrend_signal(self) -> None:
-        """Detrend the temperature, relative humidity and pressure time series."""
-        self.t_det = np.mean(self.t_raw) + detrend(self.t_raw, type="linear")
-        self.rF_det = np.mean(self.rF_raw) + detrend(self.rF_raw, type="linear")
-        self.p_det = np.mean(self.p_raw) + detrend(self.p_raw, type="linear")
+class SonicDataset(Dataset):
+    # Dataset for sonic anemometer data
 
-    def calc_spectrum(self, sample_rate: int = 1) -> None:
-        """Calculate the FFT of the temperature, relative humidity and pressure time series."""
-
-        # Calculate FFT        
-        fft_output = fft.fft(self.t_det)
-        self.freqs_t = fft.fftfreq(len(self.t_det), 1/sample_rate)
-        self.spectrum_t = np.abs(fft_output)
-
-        fft_output = fft.fft(self.rF_det)
-        self.freqs_rF = fft.fftfreq(len(self.rF_det), 1/sample_rate)
-        self.spectrum_rF = np.abs(fft_output)
-
-        fft_output = fft.fft(self.p_det)
-        self.freqs_p = fft.fftfreq(len(self.p_det), 1/sample_rate)
-        self.spectrum_p = np.abs(fft_output)
-        
-        # Remove frequencies above Nyquist frequency
-        self.nf = floor(len(self.time_raw)/2)
-        self.freqs_t = self.freqs_t[:self.nf]
-        self.freqs_rF = self.freqs_rF[:self.nf]
-        self.freqs_p = self.freqs_p[:self.nf]
-        self.spectrum_t = self.spectrum_t[:self.nf]
-        self.spectrum_rF = self.spectrum_rF[:self.nf]
-        self.spectrum_p = self.spectrum_p[:self.nf]
-      
-    def smooth_1d_arr(self) -> None:
-        """Smooth the temperature, relative humidity and pressure spectrum with a moving average filter."""
-        self.kernel_size = 12
-        kernel = np.ones(self.kernel_size) / self.kernel_size
-        self.t_spectrum_smooth = np.convolve(self.spectrum_t, kernel, mode='valid')
-        self.rF_spectrum_smooth = np.convolve(self.spectrum_rF, kernel, mode='valid')
-        self.p_spectrum_smooth = np.convolve(self.spectrum_p, kernel, mode='valid')
-    
-    def average(self, var, x: int) -> list:
-        """Calculate the average of the time series of variable var in intervals of x minutes."""
-        windowsize = x*60
-        avg = []
-        for i in range(0, len(self.time_raw), windowsize):
-            new_values = [np.mean(var[i:i+windowsize])] * windowsize
-            avg.extend(new_values)
-        return avg
-
-    # calculate the rolling average with window size x minutes having same number of data points as ds.time_raw
-    def rolling_mean(self, var, kernel_size: int) -> list:
-        """Calculate the rolling mean of the time series of variable var with a kernel size of x minutes."""
-        roll_mean = []
-        for i in range(len(var)):
-            if i < kernel_size:
-                roll_mean.append(np.mean(var[0:i+1]))
-            else:
-                roll_mean.append(np.mean(var[i-kernel_size:i]))
-        return roll_mean
-
-class SonicDataset():
     def __init__(self, fn: str, start_time: str, end_time: str) -> None:
-        """Constructor"""
-
-        self.date: str = fn[-21:-13]
-        self.start_time: str = start_time
-        self.end_time: str = end_time
-        print(f"Processing SONIC data {self.date} {self.start_time} {self.end_time}")
-        
-        self.time_raw: np.ndarray = None
+        super().__init__(fn, start_time, end_time)
         
         # wind
         self.windx_raw: np.ndarray = None
         self.windy_raw: np.ndarray = None
         self.windz_raw: np.ndarray = None
-        self.wind_total: np.ndarray = None
-        self.wind_total_det: np.ndarray = None
-        self.wind_freq: np.ndarray = None
-        self.wind_spectrum: np.ndarray = None
-        self.wind_spectrum_smooth: np.ndarray = None
+        
+        self.wind3d: np.ndarray = None
+        self.wind3d_det: np.ndarray = None
+        self.wind3dfreqs: np.ndarray = None
+        self.wind3d_spectrum: np.ndarray = None
+        self.wind3d_spectrum_smooth: np.ndarray = None
+
+        self.wind2d: np.ndarray = None
+        self.wind2d_det: np.ndarray = None
+        self.wind2dfreqs: np.ndarray = None
+        self.wind2d_spectrum: np.ndarray = None
+        self.wind2d_spectrum_smooth: np.ndarray = None
         
         # temperature
         self.t_raw: np.ndarray = None
         self.t_det: np.ndarray = None
-        self.t_freq: np.ndarray = None
+        self.t_freqs: np.ndarray = None
         self.t_spectrum: np.ndarray = None
         self.t_spectrum_smooth: np.ndarray = None
 
         self.parse_data(fn, self.start_time, self.end_time)
-        self.detrend_signal()
-        self.calc_spectrum()
-        self.smooth_1d_arr()
+        
+        self.wind3d_det = self.detrend_signal(var=self.wind3d)
+        self.wind2d_det = self.detrend_signal(var=self.wind2d)
+        self.t_det = self.detrend_signal(var=self.t_raw)
+        
+        self.wind3d_freqs, self.wind3d_spectrum = self.calc_spectrum(var=self.wind3d_det, sample_rate=1)
+        self.wind2d_freqs, self.wind2d_spectrum = self.calc_spectrum(var=self.wind2d_det, sample_rate=1)
+        self.t_freqs, self.t_spectrum = self.calc_spectrum(var=self.wind3d_det, sample_rate=1)
+        
+        self.wind2d_spectrum_smooth = self.smooth_spectrum(self.wind2d_spectrum)
+        self.wind3d_spectrum_smooth = self.smooth_spectrum(self.wind3d_spectrum)
+        self.t_spectrum_smooth = self.smooth_spectrum(self.t_spectrum)
         
     def parse_data(self, fn: str, start_time: str, end_time: str) -> None:
         """Parse the data from the dat-file."""
@@ -182,67 +189,21 @@ class SonicDataset():
         if len(df_flt) == 0:
             sys.exit(f"Temporal filtering of {self.fn} results in 0 data points.")
 
-        def calc_wind_total(row)-> float:
+        def calc_3d_wind(row)-> float:
             """Calculate the absolute wind speed from the 3 wind components."""
             return np.sqrt(row["windx"]**2 + row["windy"]**2 + row["windz"]**2)
         
-        df_flt["wind_total"] = df_flt.apply(calc_wind_total, axis=1)
+        def calc_2d_wind(row)-> float:
+            """Calculate the horizontal wind speed from the 3 wind components."""
+            return np.sqrt(row["windx"]**2 + row["windy"]**2)
+        
+        df_flt.loc[:, "wind3d"] = df_flt.apply(calc_3d_wind, axis=1)
+        df_flt.loc[:, "wind2d"] = df_flt.apply(calc_2d_wind, axis=1)
 
         self.time_raw = pd.to_datetime(df_flt["Datetime"])
         self.windx_raw = df_flt["windx"].to_numpy()
         self.windy_raw = df_flt["windy"].to_numpy()
         self.windz_raw = df_flt["windz"].to_numpy()
-        self.wind_total = df_flt["wind_total"].to_numpy()
+        self.wind3d = df_flt["wind3d"].to_numpy()
+        self.wind2d = df_flt["wind2d"].to_numpy()
         self.t_raw = df_flt["T"].to_numpy()
-        
-    def detrend_signal(self) -> None:
-        """Detrend the wind and temperature time series."""
-        self.wind_total_det = np.mean(self.wind_total) + detrend(self.wind_total, type="linear")
-        self.t_det = np.mean(self.t_raw) + detrend(self.t_raw, type="linear")
-    
-    def calc_spectrum(self, sample_rate: int = 1) -> None:
-        """Calculate the FFT of the wind and temperature time series."""
-        
-        # Calculate FFT for wind
-        fft_output = fft.fft(self.wind_total_det)
-        self.wind_freq = fft.fftfreq(len(self.wind_total_det), 1/sample_rate)
-        self.wind_spectrum = np.abs(fft_output)
-        
-        # Calculate FFT for temperature
-        fft_output = fft.fft(self.t_det)
-        self.t_freq = fft.fftfreq(len(self.t_det), 1/sample_rate)
-        self.t_spectrum = np.abs(fft_output)
-        
-        # Remove frequencies above Nyquist frequency
-        self.nf = floor(len(self.time_raw)/2)
-        self.wind_freq = self.wind_freq[:self.nf]
-        self.wind_spectrum  = self.wind_spectrum[:self.nf]
-        self.t_freq = self.t_freq[:self.nf]
-        self.t_spectrum = self.t_spectrum[:self.nf]
-        
-    def smooth_1d_arr(self) -> None:
-        """Smooth the wind and temperature spectrum with a moving average filter."""
-        self.kernel_size = 12
-        kernel = np.ones(self.kernel_size) / self.kernel_size
-        self.wind_spectrum_smooth = np.convolve(self.wind_spectrum, kernel, mode='valid')
-        self.t_spectrum_smooth = np.convolve(self.t_spectrum, kernel, mode='valid')
-
-    def average(self, var, x: int) -> list:
-        """Calculate the average of the time series of variable var in intervals of x minutes."""
-        windowsize = x*60
-        avg = []
-        for i in range(0, len(self.time_raw), windowsize):
-            new_values = [np.mean(var[i:i+windowsize])] * windowsize
-            avg.extend(new_values)
-        return avg
-
-    # calculate the rolling average with window size x minutes having same number of data points as ds.time_raw
-    def rolling_mean(self, var, kernel_size: int) -> list:
-        """Calculate the rolling mean of the time series of variable var with a kernel size of x minutes."""
-        roll_mean = []
-        for i in range(len(var)):
-            if i < kernel_size:
-                roll_mean.append(np.mean(var[0:i+1]))
-            else:
-                roll_mean.append(np.mean(var[i-kernel_size:i]))
-        return roll_mean
