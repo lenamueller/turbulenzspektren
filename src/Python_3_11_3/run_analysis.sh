106663356b61de0854ca44391ebe40064c3e5c28
Lena Mueller <lenamariemue@gmail.com>
2023-08-14 13:16:53 +0200

    Update results Using PUO

--- src/Python_3_11_3/run_analysis.sh
+++ src/Python_3_11_3/run_analysis.sh
@@ -2,12 +2,12 @@
 
 echo "Run analysis..."
 
-for measuring_situation in "ES_2023_07_08_morning" "ES_2023_07_08_noon" "ES_2023_07_08_afternoon" "ES_2023_07_11_morning" "GAS_2023_07_11_noon" "ES_2023_07_11_noon"
+for puo in "PUO_00_0" "PUO_00_1" "PUO_01" "PUO_02" "PUO_03" "PUO_04" "PUO_05" "PUO_06" "PUO_07"
 do
-    python fft.py $measuring_situation "EXPE"
-    python fft.py $measuring_situation "SONIC"
-    python averaging.py $measuring_situation "EXPE"
-    python averaging.py $measuring_situation "SONIC"
+    python fft.py $puo "EXPE"
+    python fft.py $puo "SONIC"
+    python averaging.py $puo "EXPE"
+    python averaging.py $puo "SONIC"
 done
 
 echo "Done"
