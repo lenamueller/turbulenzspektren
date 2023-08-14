e654bed948d67aa7012d1f6103ab6f02ad57f646
Lena Mueller <lenamariemue@gmail.com>
2023-08-14 11:30:29 +0200

    Move all metadata information to setup.py

--- src/Python_3_11_3/setup.py
+++ src/Python_3_11_3/setup.py
@@ -1,49 +1,100 @@
+from Datasets import ExpeDataset, SonicDataset
 
-def metadata(measuring_situation: str) -> tuple:
+
+def create_datasets():
+    expe_datasets = [
+        ExpeDataset(fn="data/2023_07_08/20230708-1329-Log.txt", start_time="2023-07-08 00:00:00", end_time="2023-07-08 23:59:00"),
+        ExpeDataset(fn="data/2023_07_11/20230711-0504-Log.txt", start_time="2023-07-11 00:00:00", end_time="2023-07-11 23:59:00"),
+        ExpeDataset(fn="data/2023_08_11/20230811-0810-Log.txt", start_time="2023-08-11 00:00:00", end_time="2023-08-11 23:59:00"),
+        ExpeDataset(fn="data/2023_08_12/20230812-0641-Log.txt", start_time="2023-08-12 00:00:00", end_time="2023-08-12 23:59:00"),
+        # todo
+        ExpeDataset(fn="data/2023_08_12/20230812-0641-Log.txt", start_time="2023-08-12 00:00:00", end_time="2023-08-12 23:59:00"),
+    ]
+    sonic_datasets = [
+        SonicDataset(fn="data/2023_07_08/TOA5_7134.Raw_2023_07_08_0923.dat", start_time="2023-07-08 00:00:00", end_time="2023-07-08 23:59:00"),
+        SonicDataset(fn="data/2023_07_11/TOA5_7134.Raw_2023_07_11_0601.dat", start_time="2023-07-11 00:00:00", end_time="2023-07-11 23:59:00"),
+        SonicDataset(fn="data/2023_08_11/TOA5_7134.Raw_2023_08_11_0924.dat", start_time="2023-08-11 00:00:00", end_time="2023-08-11 23:59:00"),
+        SonicDataset(fn="data/2023_08_12/TOA5_7134.Raw_2023_08_12_0755.dat", start_time="2023-08-12 00:00:00", end_time="2023-08-12 23:59:00"),
+        # todo
+        SonicDataset(fn="data/2023_08_12/TOA5_7134.Raw_2023_08_12_0755.dat", start_time="2023-08-12 00:00:00", end_time="2023-08-12 23:59:00"),
+    ]
+    return expe_datasets, sonic_datasets
+
+def metadata(puo: str) -> tuple:
     """Return metadata for a given measuring situation."""
 
-    match measuring_situation:
+    match puo:
         
-        # day 1
-        case "ES_2023_07_08_morning":
+        case "PUO_00_0": # no expe data
             expe_fn = "../../data/2023_07_08/20230708-1329-Log.txt"
             sonic_fn = "../../data/2023_07_08/TOA5_7134.Raw_2023_07_08_0923.dat"
             start_date = "2023-07-08 08:25:00"
             end_date =   "2023-07-08 10:10:00"
             date = "08.07.2023"
-        case "ES_2023_07_08_noon":
+            day=1
+        case "PUO_00_1": # no expe data
             expe_fn = "../../data/2023_07_08/20230708-1329-Log.txt"
             sonic_fn = "../../data/2023_07_08/TOA5_7134.Raw_2023_07_08_0923.dat"
             start_date = "2023-07-08 11:30:00"
             end_date =   "2023-07-08 12:45:00"
             date = "08.07.2023"
-        case "ES_2023_07_08_afternoon":
+            day=1
+        case "PUO_01":
             expe_fn = "../../data/2023_07_08/20230708-1329-Log.txt"
             sonic_fn = "../../data/2023_07_08/TOA5_7134.Raw_2023_07_08_0923.dat"
             start_date = "2023-07-08 13:30:00"
             end_date =   "2023-07-08 16:10:00"
             date = "08.07.2023"
-        
-        # day 2
-        case "ES_2023_07_11_morning":
+            day=1
+        case "PUO_02":
             expe_fn = "../../data/2023_07_11/20230711-0504-Log.txt"
             sonic_fn = "../../data/2023_07_11/TOA5_7134.Raw_2023_07_11_0601.dat"
             start_date = "2023-07-11 05:05:00"
             end_date =   "2023-07-11 07:00:00"
-            date = "09.07.2023"
-        case "GAS_2023_07_11_noon":
+            date = "11.07.2023"
+            day=2
+        case "PUO_03":
             expe_fn = "../../data/2023_07_11/20230711-0504-Log.txt"
             sonic_fn = "../../data/2023_07_11/TOA5_7134.Raw_2023_07_11_0601.dat"
             start_date = "2023-07-11 10:00:00"
             end_date =   "2023-07-11 11:00:00"
             date = "11.07.2023"
-        case "ES_2023_07_11_noon":
+            day=2
+        case "PUO_04":
             expe_fn = "../../data/2023_07_11/20230711-0504-Log.txt"
             sonic_fn = "../../data/2023_07_11/TOA5_7134.Raw_2023_07_11_0601.dat"
             start_date = "2023-07-11 11:36:00"
             end_date =   "2023-07-11 12:36:00"
             date = "11.07.2023"
-    
-        # TODO: #1 update metadata for day 3
+            day=2
+        case "PUO_05":
+            expe_fn = "../../data/2023_08_11/20230811-0810-Log.txt"
+            sonic_fn = "../../data/2023_08_11/TOA5_7134.Raw_2023_08_11_0924.dat"
+            start_date = "2023-08-11 08:25:00"
+            end_date =   "2023-08-11 09:55:00"
+            date = "11.08.2023"
+            day=3
+        case "PUO_06":
+            expe_fn = "../../data/2023_08_11/20230811-0810-Log.txt"
+            sonic_fn = "../../data/2023_08_11/TOA5_7134.Raw_2023_08_11_0924.dat"
+            start_date = "2023-08-11 12:20:00"
+            end_date =   "2023-08-11 16:50:00"
+            date = "11.08.2023"
+            day=3
+        case "PUO_07":
+            expe_fn = "../../data/2023_08_12/20230812-0641-Log.txt"
+            sonic_fn = "../../data/2023_08_12/TOA5_7134.Raw_2023_08_12_0755.dat"
+            start_date = "2023-08-12 06:55:00"
+            end_date = "2023-08-12 14:20:00"
+            date = "12.08.2023"
+            day=4
+        # TODO
+        case "PUO_08":
+            expe_fn = "../../data/2023_08_12/20230812-0641-Log.txt"
+            sonic_fn = "../../data/2023_08_12/TOA5_7134.Raw_2023_08_12_0755.dat"
+            start_date = "2023-08-14 07:00:00"
+            end_date =   "2023-08-14 07:00:00"
+            date = "14.08.2023"
+            day=5
         
-    return expe_fn, sonic_fn, start_date, end_date, date
+    return expe_fn, sonic_fn, start_date, end_date, date, day
--- src/Python_3_11_3/temporal_coverage.py
+++ src/Python_3_11_3/temporal_coverage.py
@@ -1,50 +1,41 @@
 import matplotlib.pyplot as plt
 from matplotlib.dates import DateFormatter
 import pandas as pd
+import os
 
 from Datasets import ExpeDataset, SonicDataset
+from setup import metadata, create_datasets
 
+
+puos = ["PUO_00_0", "PUO_00_1", "PUO_01", "PUO_02", "PUO_03", 
+        "PUO_04", "PUO_05", "PUO_06", "PUO_07", "PUO_08"]
+
+# get unique dates from metadata
+dates = []
+for puo in puos:
+    _, _, _, _, date, day = metadata(puo)
+    if date not in dates:
+        dates.append(date)
 
 # create Dataset objects
-expe_datasets = [
-    ExpeDataset(fn="data/2023_07_08/20230708-1329-Log.txt", start_time="2023-07-08 00:00:00", end_time="2023-07-08 23:59:00"),
-    ExpeDataset(fn="data/2023_07_11/20230711-0504-Log.txt", start_time="2023-07-11 00:00:00", end_time="2023-07-11 23:59:00"),
-    ExpeDataset(fn="data/2023_08_11/20230811-0810-Log.txt", start_time="2023-08-11 00:00:00", end_time="2023-08-11 23:59:00"),
-    ExpeDataset(fn="data/2023_08_12/20230812-0641-Log.txt", start_time="2023-08-12 00:00:00", end_time="2023-08-12 23:59:00"),
-    # todo
-    ExpeDataset(fn="data/2023_08_12/20230812-0641-Log.txt", start_time="2023-08-12 00:00:00", end_time="2023-08-12 23:59:00"),
-]
-sonic_datasets = [
-    SonicDataset(fn="data/2023_07_08/TOA5_7134.Raw_2023_07_08_0923.dat", start_time="2023-07-08 00:00:00", end_time="2023-07-08 23:59:00"),
-    SonicDataset(fn="data/2023_07_11/TOA5_7134.Raw_2023_07_11_0601.dat", start_time="2023-07-11 00:00:00", end_time="2023-07-11 23:59:00"),
-    SonicDataset(fn="data/2023_08_11/TOA5_7134.Raw_2023_08_11_0924.dat", start_time="2023-08-11 00:00:00", end_time="2023-08-11 23:59:00"),
-    SonicDataset(fn="data/2023_08_12/TOA5_7134.Raw_2023_08_12_0755.dat", start_time="2023-08-12 00:00:00", end_time="2023-08-12 23:59:00"),
-    # todo
-    SonicDataset(fn="data/2023_08_12/TOA5_7134.Raw_2023_08_12_0755.dat", start_time="2023-08-12 00:00:00", end_time="2023-08-12 23:59:00"),
-]
+expe_datasets, sonic_datasets = create_datasets()
 
-dates = ["08.07.2023", "11.07.2023", "11.08.2023", "12.08.2023", "14.08.2023"]
+# get PUO ranges
+ranges = []
+for puo in puos[2:]: # skip first two puos without expe data
+    _, _, start_date, end_date, _, day = metadata(puo)
+    start = pd.to_datetime(start_date, format="%Y-%m-%d %H:%M:%S")
+    end = pd.to_datetime(end_date, format="%Y-%m-%d %H:%M:%S")
+    ranges.append((day-1, start, end))
 
-ranges = [
-    (0, pd.Timestamp(2023, 7, 8, 13, 30), pd.Timestamp(2023, 7, 8, 16, 10)),
-    (1, pd.Timestamp(2023, 7, 11, 5, 5), pd.Timestamp(2023, 7, 11, 7, 0)),
-    (1, pd.Timestamp(2023, 7, 11, 10, 0), pd.Timestamp(2023, 7, 11, 11, 0)),
-    (1, pd.Timestamp(2023, 7, 11, 11, 36), pd.Timestamp(2023, 7, 11, 12, 36)),
-    (2, pd.Timestamp(2023, 8, 11, 8, 25), pd.Timestamp(2023, 8, 11, 9, 55)),
-    (2, pd.Timestamp(2023, 8, 11, 12, 20), pd.Timestamp(2023, 8, 11, 16, 50)),
-    (3, pd.Timestamp(2023, 8, 12, 6, 55), pd.Timestamp(2023, 8, 12, 14, 20)),
-    # todo
-    (4, pd.Timestamp(2023, 8, 12, 6, 55), pd.Timestamp(2023, 8, 12, 14, 20))
-]
-
+# plot
 fig, ax = plt.subplots(nrows=len(dates), ncols=1, figsize=(15,20))
 for i in range(len(dates)):
     
-    # legend entries
-    lns = []
+    lns = [] # legend entries
     
     # PUO = period under observation
-    for j in range(len(ranges)):
+    for j in range(len(ranges)): 
         ax_i, start, end = ranges[j]
         if ax_i == i:
             hours = round((end-start).total_seconds()/(60*60), 1)
