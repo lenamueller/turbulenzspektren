63181ab52e2a56b159ad9cdf4a3b11fb069f7310
Lena Mueller <lenamariemue@gmail.com>
2023-11-19 19:06:08 +0100

    📈 Plotting spectra

--- src/Python_3_11_3/plot.py
+++ src/Python_3_11_3/plot.py
@@ -2,12 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.dates import DateFormatter
 
-from process import detrend_signal, taper_signal
+from process import detrend_signal, taper_signal, calc_spectrum, smooth, roll_mean
 
 
-grid_kwargs = {"color":"lightgrey", "lw":0.4}
-line_kwargs = {"color":"mediumblue", "lw":0.6}
-title_kwargs = {"fontweight":"bold", "fontsize":12, "color":"grey"}
+grid_kwargs =           {"color":"lightgrey", "lw":0.4}
+line_kwargs =           {"color":"mediumblue", "lw":0.6}
+smooth_spec_kw_args =   {"lw": 1.0, "alpha": 0.5, "c": "r"}
+title_kwargs =          {"fontweight":"bold", "fontsize":12, "color":"grey"}
+scat_kw_args =          {"s": 1.0, "alpha": 0.6, "c": "darkgrey"}
+range_kw_args =         {"alpha": 0.1, "color": "orange"}
+    
 
 def plot_ts(
         x: np.ndarray, y: np.ndarray,
@@ -35,9 +39,41 @@ def plot_ts(
     
     plt.savefig(f"plots/preprocessing/preprocess_{fn}.png", dpi=300, bbox_inches="tight")
 
-def plot_spectrum():
+def plot_spectrum(
+        x: np.ndarray, y: np.ndarray,
+        fn: str, ylabel: str, title: str
+        ) -> None:
     """Plots the spectrum of a time series."""
-    pass
+
+    y_tapered = taper_signal(detrend_signal(y), 0.1)
+    freq, spec = calc_spectrum(x, y_tapered)
+    
+    fig, ax = plt.subplots(nrows=2, ncols=1, figsize=(10,7))
+    fig.suptitle(title, **title_kwargs)
+    
+    ax[0].plot(x, y_tapered, label="Edited time series", **line_kwargs)
+    ax[1].scatter(freq, spec, label="Spectrum", **scat_kw_args)
+    ax[1].plot(freq, roll_mean(spec, win_len=10), label="Rolling mean", **smooth_spec_kw_args)
+    ax[1].axvspan(1/(60*30), 1/(60*60), label="30 min - 60 min", **range_kw_args)
+    
+    ax[0].xaxis.set_major_formatter(DateFormatter('%H:%M'))
+    ax[0].set_xlim(x[0], x[-1])
+    ax[0].set_xlabel("Time [UTC]")
+    ax[0].set_ylabel(ylabel)
+    ax[1].set_xlabel("Frequency [Hz]")
+    ax[1].set_ylabel("Spectral Energy Density * Frequency")
+    ax[1].set_xscale("log")
+    ax[1].set_xlim((1e-4, 1e-1))
+    ax[1].set_xticks([1e-4, 1e-3, 1e-2, 1e-1])
+    ax2 = ax[1].secondary_xaxis(-0.2, functions=(lambda x: 1/x, lambda x: 1/x))
+    ax2.set_xticks([10000, 1000, 100, 10])
+    ax2.set_xlabel("Period [s]")
+    
+    for i in [0,1]:
+        ax[i].grid(True)
+        ax[i].legend(loc="upper left")
+        
+    plt.savefig(f"plots/spectra/spec_{fn}.png", dpi=300, bbox_inches="tight")
 
 def plot_spectrum_comp():
     """Plots a comparison of all smoothed spectra."""
