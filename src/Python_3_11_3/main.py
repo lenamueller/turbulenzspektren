36c21d991f1257be67d60fc0f467b6bf86253bda
Lena Mueller <lenamariemue@gmail.com>
2023-11-26 18:06:40 +0100

    🗃️ Rename spectra data files

--- src/Python_3_11_3/main.py
+++ src/Python_3_11_3/main.py
@@ -13,126 +13,118 @@ from plot import plot_ts, plot_spectrum, plot_spectrum_comp, plot_t_spectrum_com
 
 
 # -----------------------------------------------------------------------------
-# calculations
+# process EXPE data
 # -----------------------------------------------------------------------------
 
-print("Run calculations...")
-
+print("Process EXPE data...")
 for period in all_puos:
-    for device in ["SONIC", "EXPE"]:
-        print(period, device)
-        data = parse_data(device, period)
-        
-        match device:
-            case "EXPE":
-                
-                # load data
-                dt = data["Datetime"].to_numpy()
-                t = data["t"].to_numpy()
-                rh = data["rh"].to_numpy()
-                p = data["p"].to_numpy()
-                
-                # time series data
-                timeseries_data = {}
-                timeseries_data["datetime"] = dt
-                timeseries_data["t"] = t
-                timeseries_data["rh"] = rh
-                timeseries_data["p"] = p
-                timeseries_data["t_det"] = detrend_signal(t)
-                timeseries_data["rh_det"] = detrend_signal(rh)
-                timeseries_data["p_det"] = detrend_signal(p)
-                timeseries_data["t_tap"] = taper_signal(timeseries_data["t_det"], TAPERING_SIZE)
-                timeseries_data["rh_tap"] = taper_signal(timeseries_data["rh_det"], TAPERING_SIZE)
-                timeseries_data["p_tap"] = taper_signal(timeseries_data["p_det"], TAPERING_SIZE)
-                pd.DataFrame.from_dict(timeseries_data).to_csv(
-                    f"data/timeseries_data/{period}_{device}_preprocessed_data.csv", index=False)
-                
-                # spectrum data
-                spectrum_data = {}
-                spectrum_data["frequencies"] = sample_freq(dt)
-                spectrum_data["t_spec"] = calc_spectrum(dt, timeseries_data["t_tap"])[1]
-                spectrum_data["rh_spec"] = calc_spectrum(dt, timeseries_data["rh_tap"])[1]
-                spectrum_data["p_spec"] = calc_spectrum(dt, timeseries_data["p_tap"])[1]
-                spectrum_data["t_spec_smooth"] = roll_mean(spectrum_data["t_spec"], win_len=KERNEL_SIZE)
-                spectrum_data["rh_spec_smooth"] = roll_mean(spectrum_data["rh_spec"], win_len=KERNEL_SIZE)
-                spectrum_data["p_spec_smooth"] = roll_mean(spectrum_data["p_spec"], win_len=KERNEL_SIZE)
-                pd.DataFrame.from_dict(spectrum_data).to_csv(
-                        f"data/spectra_data/{period}_{device}_spectrum_data.csv", index=False)
-                
-                # calculate turbulence intensity
-                for var in ["t", "rh", "p"]:
-                    print(f"\t\tTurbulent intensity of {var}: {calc_turb_int(y=timeseries_data[var])}")
-            
-            case "SONIC":
 
-                # load data
-                dt = data["Datetime"].to_numpy()
-                t = data["t"].to_numpy()
-                wind3d = data["wind3d"].to_numpy()
-                wind2d = data["wind2d"].to_numpy()
-                
-                # time series data
-                timeseries_data = {}
-                timeseries_data["datetime"] = dt
-                timeseries_data["t"] = t
-                timeseries_data["wind3d"] = wind3d
-                timeseries_data["wind2d"] = wind2d
-                timeseries_data["t_det"] = detrend_signal(t)
-                timeseries_data["wind3d_det"] = detrend_signal(wind3d)
-                timeseries_data["wind2d_det"] = detrend_signal(wind2d)
-                timeseries_data["t_tap"] = taper_signal(timeseries_data["t_det"], TAPERING_SIZE)
-                timeseries_data["wind3d_tap"] = taper_signal(timeseries_data["wind3d_det"], TAPERING_SIZE)
-                timeseries_data["wind2d_tap"] = taper_signal(timeseries_data["wind2d_det"], TAPERING_SIZE)
-                pd.DataFrame.from_dict(timeseries_data).to_csv(
-                    f"data/timeseries_data/{period}_{device}_preprocessed_data.csv", index=False)
-                
-                # spectrum data
-                spectrum_data = {}
-                spectrum_data["frequencies"] = sample_freq(dt)
-                spectrum_data["t_spec"] = calc_spectrum(dt, timeseries_data["t_tap"])[1]
-                spectrum_data["wind3d_spec"] = calc_spectrum(dt, timeseries_data["wind3d_tap"])[1]
-                spectrum_data["wind2d_spec"] = calc_spectrum(dt, timeseries_data["wind2d_tap"])[1]
-                spectrum_data["t_spec_smooth"] = roll_mean(spectrum_data["t_spec"], win_len=KERNEL_SIZE)
-                spectrum_data["wind3d_spec_smooth"] = roll_mean(spectrum_data["wind3d_spec"], win_len=KERNEL_SIZE)
-                spectrum_data["wind2d_spec_smooth"] = roll_mean(spectrum_data["wind2d_spec"], win_len=KERNEL_SIZE)
-                pd.DataFrame.from_dict(spectrum_data).to_csv(
-                        f"data/spectra_data/{period}_{device}_spectrum_data.csv", index=False)
+    # load data
+    data = parse_data("EXPE", period)
+    dt = data["Datetime"].to_numpy()
+    t = data["t"].to_numpy()
+    
+    # calculate detrended and tapered time series
+    timeseries_data = {}
+    timeseries_data["datetime"] = dt
+    timeseries_data["t"] = t
+    timeseries_data["t_det"] = detrend_signal(t)
+    timeseries_data["t_tap"] = taper_signal(timeseries_data["t_det"], TAPERING_SIZE)
+    
+    pd.DataFrame.from_dict(timeseries_data).to_csv(
+        f"data/timeseries_data/{period}_EXPE_preprocessed_data.csv", index=False)
+    
+    # calculate spectrum and smooth it with rolling mean
+    spectrum_data = {}
+    spectrum_data["frequencies"] = sample_freq(dt)
+    spectrum_data["t_spec"] = calc_spectrum(dt, timeseries_data["t_tap"])[1]
+    spectrum_data["t_spec_smooth"] = roll_mean(spectrum_data["t_spec"], win_len=KERNEL_SIZE)
+    
+    pd.DataFrame.from_dict(spectrum_data).to_csv(
+            f"data/spectra_data/{period}_EXPE_spectrum_data.csv", index=False)
 
-                # calculate turbulence intensity
-                for var in ["t", "wind3d", "wind2d"]:
-                    print(f"\t\tTurbulent intensity of {var}: {calc_turb_int(y=timeseries_data[var])}")
-                    
+# -----------------------------------------------------------------------------
+# process SONIC data
+# -----------------------------------------------------------------------------
+
+print("Process SONIC data...")
 for period in all_puos:
+    
+    # load data
+    data = parse_data("SONIC", period)
+    dt = data["Datetime"].to_numpy()
+    t = data["t"].to_numpy()
+    wind_z = data["wind_z"].to_numpy()
+    wind_h = data["wind_h"].to_numpy()
+    
+    # calculate detrended and tapered time series
+    timeseries_data = {}
+    timeseries_data["datetime"] = dt
+    
+    timeseries_data["t"] = t
+    timeseries_data["t_det"] = detrend_signal(t)
+    timeseries_data["t_tap"] = taper_signal(timeseries_data["t_det"], TAPERING_SIZE)
+    
+    timeseries_data["wind_z"] = wind_z
+    timeseries_data["wind_z_det"] = detrend_signal(wind_z)
+    timeseries_data["wind_z_tap"] = taper_signal(timeseries_data["wind_z_det"], TAPERING_SIZE)
+    
+    timeseries_data["wind_h"] = wind_h
+    timeseries_data["wind_h_det"] = detrend_signal(wind_h)
+    timeseries_data["wind_h_tap"] = taper_signal(timeseries_data["wind_h_det"], TAPERING_SIZE)
+    
+    pd.DataFrame.from_dict(timeseries_data).to_csv(
+        f"data/timeseries_data/{period}_SONIC_preprocessed_data.csv", index=False)
+    
+    # calculate spectrum and smooth it with rolling mean
+    spectrum_data = {}
+    spectrum_data["frequencies"] = sample_freq(dt)
+    
+    spectrum_data["t_spec"] = calc_spectrum(dt, timeseries_data["t_tap"])[1]
+    spectrum_data["t_spec_smooth"] = roll_mean(spectrum_data["t_spec"], win_len=KERNEL_SIZE)
+    
+    spectrum_data["wind_z_spec"] = calc_spectrum(dt, timeseries_data["wind_z_tap"])[1]
+    spectrum_data["wind_z_spec_smooth"] = roll_mean(spectrum_data["wind_z_spec"], win_len=KERNEL_SIZE)
+    
+    spectrum_data["wind_h_spec"] = calc_spectrum(dt, timeseries_data["wind_h_tap"])[1]
+    spectrum_data["wind_h_spec_smooth"] = roll_mean(spectrum_data["wind_h_spec"], win_len=KERNEL_SIZE)
+    
+    pd.DataFrame.from_dict(spectrum_data).to_csv(
+            f"data/spectra_data/{period}_SONIC_spectrum_data.csv", index=False)
+
+# -----------------------------------------------------------------------------
+# process EXPE and SONIC data and store them in one file
+# -----------------------------------------------------------------------------
+
+print("Process EXPE and SONIC data...")
+for period in all_puos:
+    
     comparison = pd.DataFrame()    
     
     # EXPE
     data = parse_data("EXPE", period)
     dt = data["Datetime"].to_numpy()
     t = data["t"].to_numpy()
-    rh = data["rh"].to_numpy()
-    p = data["p"].to_numpy()
     
     comparison["frequencies"] = calc_spectrum(dt, taper_signal(detrend_signal(t), perc=0.1))[0]
-    n_expe = len(comparison)
-    
     comparison["EXPE_t"] = calc_spectrum(dt, taper_signal(detrend_signal(t), perc=0.1))[1]
-    comparison["EXPE_rh"] = calc_spectrum(dt, taper_signal(detrend_signal(rh), perc=0.1))[1]
-    comparison["EXPE_p"] = calc_spectrum(dt, taper_signal(detrend_signal(p), perc=0.1))[1]
+    
+    n_expe = len(comparison)
     
     # SONIC
     data = parse_data("SONIC", period)
-    
     dt = data["Datetime"].to_numpy()
     t = data["t"].to_numpy()
-    wind3d = data["wind3d"].to_numpy()
-    wind2d = data["wind2d"].to_numpy()
+    wind_z = data["wind_z"].to_numpy()
+    wind_h = data["wind_h"].to_numpy()
     
     comparison["SONIC_t"] = calc_spectrum(dt, taper_signal(detrend_signal(t), perc=0.1))[1][:n_expe]
-    comparison["SONIC_wind3d"] = calc_spectrum(dt, taper_signal(detrend_signal(wind3d), perc=0.1))[1][:n_expe]
-    comparison["SONIC_wind2d"] = calc_spectrum(dt, taper_signal(detrend_signal(wind2d), perc=0.1))[1][:n_expe]
+    comparison["SONIC_wind_z"] = calc_spectrum(dt, taper_signal(detrend_signal(wind_z), perc=0.1))[1][:n_expe]
+    comparison["SONIC_wind_h"] = calc_spectrum(dt, taper_signal(detrend_signal(wind_h), perc=0.1))[1][:n_expe]
         
     # save to csv
-    comparison.to_csv(f"data/spectra_data/comparison_{period}.csv", index=False)
+    comparison.to_csv(f"data/spectra_data/{period}_comparison_spectrum_data.csv", index=False)
+
         
 # -----------------------------------------------------------------------------
 # plotting
